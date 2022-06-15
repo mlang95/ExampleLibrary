4e01541b649a080f3449b9d5d54645e38bc6f9d4
mlang95 <mattlang95@gmail.com>
2022-06-15 17:33:54 -0400

    Create setup.py
    create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+  name = "levenshtein",
+  packages = find_packages(),
+  version = "0.1.0",
+  description = "Levenshtein is used to demonstrate how similar two strings are",
+  license = "",
+  install_requires = [],
+  setup_requires = [],
+  tests_requires = [],
+  test_suite = "tests",
+)
